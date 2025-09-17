9a3ca8791094b27867368cbb6eb0b2cff4d84256
Inna Z <45328865+udarmolota@users.noreply.github.com>
2025-09-17 21:06:17 +0300

    Update ver num

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
         minSdk = 30
         targetSdk = 35
         versionCode = 3
-        versionName = "1.2.0"
+        versionName = "1.2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
