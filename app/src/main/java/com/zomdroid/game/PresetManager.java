a5adb36765ecf5506367fe27058806729199749b
Inna Z <45328865+udarmolota@users.noreply.github.com>
2025-09-17 19:54:29 +0300

    Fix #3

--- app/src/main/java/com/zomdroid/game/PresetManager.java
+++ app/src/main/java/com/zomdroid/game/PresetManager.java
@@ -36,10 +36,13 @@ public class PresetManager {
                         "core-3.2.1.jar"
                 })
                 .setExtraJars(new String[0])
-                .setLibraryPathArray(new String[0]
-                        //{C.deps.LIBS_ANDROID_ARM64_v8a,
-                        {C.deps.LIBS_LWJGL_336}
-                )
+                .setLibraryPathArray(new String[]{
+                    C.deps.LIBS_LWJGL_336
+                })
+                //.setLibraryPathArray(new String[0]{
+                //        C.deps.LIBS_ANDROID_ARM64_v8a,
+                //        C.deps.LIBS_LWJGL_336
+                //})
                 .setLibraryPathForEmulationArray(new String[]{
                         C.deps.LIBS_LINUX_X86_64
                 })
