6429e57aa387b1770e4dd1d4c68321b18a28739e
Inna Z <45328865+udarmolota@users.noreply.github.com>
2025-09-20 16:55:32 +0300

    Delete app/src/main/cpp/InputNativeInterface.cpp

--- app/src/main/cpp/InputNativeInterface.cpp
+++
@@ -1,8 +0,0 @@
-#include <jni.h>
-
-extern "C"
-JNIEXPORT void JNICALL
-Java_com_zomdroid_input_InputNativeInterface_1sendJoystickDpad(
-        JNIEnv* /*env*/, jclass /*clazz*/, jint /*dpad*/, jchar /*state*/) {
-    // no-op
-}
