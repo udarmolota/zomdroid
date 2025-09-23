d43e9a4538ba53cbbd9c22812ff373a63e4f2ef6
Inna Z <45328865+udarmolota@users.noreply.github.com>
2025-09-23 20:48:34 +0300

    D-pad better support

--- app/src/main/cpp/zomdroid_jni.c
+++ app/src/main/cpp/zomdroid_jni.c
@@ -110,9 +110,14 @@ Java_com_zomdroid_input_InputNativeInterface_sendJoystickAxis(JNIEnv *env, jclas
 }
 
 JNIEXPORT void JNICALL
-Java_com_zomdroid_input_InputNativeInterface_sendJoystickDpad(JNIEnv *env, jclass clazz, jint dpad,
-                                               jchar state) {
-   zomdroid_event_joystick_dpad((int)dpad, (char)state);
+Java_com_zomdroid_input_InputNativeInterface_sendJoystickAxis(JNIEnv *env, jclass clazz, jint axis, jfloat state) {
+    zomdroid_event_joystick_axis(axis, state);
+}
+
+JNIEXPORT void JNICALL
+Java_com_zomdroid_input_InputNativeInterface_sendJoystickButton(JNIEnv *env, jclass clazz, jint button,
+jboolean pressed) {
+    zomdroid_event_joystick_button(button, pressed);
 }
 
 JNIEXPORT void JNICALL
