629c7f3f75fe11ef878c18dfcc0ac9fa324db954
Inna Z <45328865+udarmolota@users.noreply.github.com>
2025-09-24 14:00:53 +0300

    Logger changed

--- app/src/main/java/com/zomdroid/input/InputNativeInterface.java
+++ app/src/main/java/com/zomdroid/input/InputNativeInterface.java
@@ -11,7 +11,11 @@ public class InputNativeInterface {
 
     //public static native void sendJoystickDpad(int dpad, char state);
 
-    public static native void sendJoystickButton(int button, boolean isPressed);
+    public static void debugSendJoystickButton(int button, boolean isPressed) {
+        System.out.println("InputNativeInterface: sendJoystickButton(" + button + ", " + isPressed + ")");
+        sendJoystickButton(button, isPressed);
+    }
+
 
     public static native void sendJoystickConnected();
 }
