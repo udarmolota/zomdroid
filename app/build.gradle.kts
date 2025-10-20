d63a1f1529058a2ba38edf72d61772e03bf1eb62
Inna Z <45328865+udarmolota@users.noreply.github.com>
2025-10-20 13:10:08 +0300

    change ver num

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
         minSdk = 30
         targetSdk = 35
         versionCode = 4
-        versionName = "1.2.4.v2"
+        versionName = "1.2.4.v5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
