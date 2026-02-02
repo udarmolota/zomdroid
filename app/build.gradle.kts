6950781407b29ac627181c3468071b0c9f865f73
udarmolota <inna.zengin@gmail.com>
2026-02-03 00:09:58 +0200

    v1.3.2 new Java25 + Freedreno 8xx + FMOD.jar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
         minSdk = 30
         targetSdk = 35
         versionCode = 6
-        versionName = "1.3.1"
+        versionName = "1.3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
