abb71b384b1729d373d1b7453cb02605b75d665e
Inna Z <45328865+udarmolota@users.noreply.github.com>
2026-02-19 18:44:41 +0200

    Added sendChar

--- app/src/main/java/com/zomdroid/input/InputNativeInterface.java
+++ app/src/main/java/com/zomdroid/input/InputNativeInterface.java
@@ -8,4 +8,5 @@ public class InputNativeInterface {
     public static native void sendJoystickDpad(int dpad, char state);
     public static native void sendJoystickButton(int button, boolean isPressed);
     public static native void sendJoystickConnected();
+    public static native void sendChar(int codepoint);
 }
