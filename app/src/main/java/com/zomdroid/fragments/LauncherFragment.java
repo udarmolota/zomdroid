0b0a99425a3a63b76eac9e66075dcefade1abcc5
Inna Z <45328865+udarmolota@users.noreply.github.com>
2026-04-12 18:39:31 +0300

    Banner selection

--- app/src/main/java/com/zomdroid/fragments/LauncherFragment.java
+++ app/src/main/java/com/zomdroid/fragments/LauncherFragment.java
@@ -139,6 +139,22 @@ public class LauncherFragment extends Fragment {
 
                 nameTv.setText(gameInstance.getName());
 
+                ImageView bannerIv = itemView.findViewById(R.id.game_instance_item_banner_iv);
+                String presetName = gameInstance.getPreset().name;
+                int bannerRes;
+                switch (presetName) {
+                    case "Build 42.12+":
+                        bannerRes = R.drawable.banner_build42_12;
+                        break;
+                    case "Build 42":
+                        bannerRes = R.drawable.banner_build42;
+                        break;
+                    default:
+                        bannerRes = R.drawable.banner_build41;
+                        break;
+                }
+                bannerIv.setImageResource(bannerRes);
+
                 launchIb.setOnClickListener(v -> {
                     if (!gameInstance.isInstallationFinished()) {
                         Toast.makeText(getContext(), R.string.installation_not_finished,
